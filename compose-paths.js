fea0545437d8ba51ee688011d40443e2b05a1c0d
Jim Kang <jimkang@gmail.com>
2015-04-08 10:45:54 -0400

    Moved composePaths to its own module. Stopped filtering for judgeability.

---
+++ compose-paths.js
@@ -0,0 +1,51 @@
+var _ = require('lodash');
+var edgeFilters = require('./edge-filters');
+
+function composePaths(rootConceptUri, primaryEdge, secondaryEdges) {
+  var startConceptUri = primaryEdge.start;
+  var endConceptUri = primaryEdge.end;
+
+  // var secondaryJudgeableEdges = edgeFilters.filterToJudgeableEdges(
+  //   secondaryEdges, endConceptUri
+  // );
+  secondaryEdges = edgeFilters.filterConceptOutOfEdges(
+    secondaryEdges, rootConceptUri
+  );
+
+  var pathBase = [
+    {
+      newConcept: rootConceptUri
+    },
+    {
+      vector: {
+        rel: primaryEdge.rel,
+        start: startConceptUri,
+        end: endConceptUri
+      },
+      newConcept: notX(rootConceptUri, [startConceptUri, endConceptUri])
+    }
+  ];
+
+  var paths = secondaryEdges.map(addToPath);
+
+  function addToPath(secondaryEdge) {
+    var path = _.cloneDeep(pathBase);
+    var pathNode = {
+      vector: _.pick(secondaryEdge, 'start', 'rel', 'end'),
+      newConcept: notX(
+        pathBase[1].newConcept, [secondaryEdge.start, secondaryEdge.end]
+      )
+    };
+    path.push(pathNode);
+    return path;
+  }
+
+  return paths;
+}
+
+// Assumes either pair[0] == x or pair[1] === x.
+function notX(x, pair) {
+  return (pair[0] === x) ? pair[1] : pair[0];
+}
+
+module.exports = composePaths;
--- tools/try-it.js
+++ tools/try-it.js
@@ -3,6 +3,7 @@ var async = require('async');
 var _ = require('lodash');
 var callBackOnNextTick = require('conform-async').callBackOnNextTick;
 var edgeFilters = require('../edge-filters');
+var composePaths = require('../compose-paths');
 // var queue = require('queue-async');
 
 var cmdOpts = require('nomnom').parse();
@@ -56,56 +57,10 @@ function getPrimaryEdgeEndJudgmentPath(primaryEdge, done) {
     else {
       done(
         error,
-        composeJudgementPath(primaryEdge, secondaryConcept.edges)
+        composePaths(rootConceptUri, primaryEdge, secondaryConcept.edges)
       );
     }
   }
-}
-
-function composeJudgementPath(primaryEdge, secondaryEdges) {
-  var startConceptUri = primaryEdge.start;
-  var endConceptUri = primaryEdge.end;
-
-  var secondaryJudgeableEdges = edgeFilters.filterToJudgeableEdges(
-    secondaryEdges, endConceptUri
-  );
-  secondaryJudgeableEdges = edgeFilters.filterConceptOutOfEdges(
-    secondaryEdges, rootConceptUri
-  );
-
-  var pathBase = [
-    {
-      newConcept: rootConceptUri
-    },
-    {
-      vector: {
-        rel: primaryEdge.rel,
-        start: startConceptUri,
-        end: endConceptUri
-      },
-      newConcept: notX(rootConceptUri, [startConceptUri, endConceptUri])
-    }
-  ];
-
-  var paths = secondaryJudgeableEdges.map(addToPath);
-  function addToPath(secondaryEdge) {
-    var path = _.cloneDeep(pathBase);
-    var pathNode = {
-      vector: _.pick(secondaryEdge, 'start', 'rel', 'end'),
-      newConcept: notX(
-        pathBase[1].newConcept, [secondaryEdge.start, secondaryEdge.end]
-      )
-    };
-    path.push(pathNode);
-    return path;
-  }
-
-  return paths;
-}
-
-// Assumes either pair[0] == x or pair[1] === x.
-function notX(x, pair) {
-  return (pair[0] === x) ? pair[1] : pair[0];
 }
 
 function logConceptInfo(info) {
